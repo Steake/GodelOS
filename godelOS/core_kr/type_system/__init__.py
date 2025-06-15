9a045cadf5141625c4688e36f7e36cb44175769e
Oli <ollie@olliehirst.com>
2025-06-15 08:28:36 +0700

    fix missing dependencies and add type visitor

--- godelOS/core_kr/type_system/__init__.py
+++ godelOS/core_kr/type_system/__init__.py
@@ -4,21 +4,15 @@ Type System Manager.
 This module defines and manages the type hierarchy and performs type checking and inference.
 """
 
-from godelOS.core_kr.type_system.types import (
-    Type,
-    AtomicType,
-    FunctionType,
-    TypeVariable,
-    ParametricTypeConstructor,
-    InstantiatedParametricType,
-)
-from godelOS.core_kr.type_system.manager import TypeSystemManager
 from godelOS.core_kr.type_system.environment import TypeEnvironment
-from godelOS.core_kr.type_system.visitor import (
-    Error,
-    TypeInferenceVisitor,
-    TypeCheckingVisitor,
-)
+from godelOS.core_kr.type_system.manager import TypeSystemManager
+from godelOS.core_kr.type_system.types import (AtomicType, FunctionType,
+                                               InstantiatedParametricType,
+                                               ParametricTypeConstructor, Type,
+                                               TypeVariable)
+from godelOS.core_kr.type_system.visitor import (Error, TypeCheckingVisitor,
+                                                 TypeInferenceVisitor,
+                                                 TypeVisitor)
 
 __all__ = [
     "Type",
@@ -32,4 +26,5 @@ __all__ = [
     "Error",
     "TypeInferenceVisitor",
     "TypeCheckingVisitor",
+    "TypeVisitor",
 ]
